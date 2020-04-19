4025cb049aedb1b2e1e44eb6d4b585ab433c50a1
Uriel Arriaga <joarriag@cisco.com>
2020-04-19 02:01:42 -0500

    corrected unittest for respuesta

--- test_HTTP_Codes.py
+++ test_HTTP_Codes.py
@@ -22,7 +22,7 @@ class MainTestCase(unittest.TestCase):
     def test_repuestateleconsulta(self):
         tester = app.test_client(self)
         response = tester.get("/repuestateleconsulta",content_type="html/text")
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':

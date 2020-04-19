30a9302bb58af863394f07bd38c569608a1cc046
Uriel Arriaga <joarriag@cisco.com>
2020-04-19 12:50:16 -0500

    add functional Login

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 import flask, requests, json
-from flask import request, redirect, url_for
+from flask import request, redirect, url_for, render_template, flash
 from funciones import sendWebexMsg, sendSMS
+from forms import LoginForm
 
 
 
 app = flask.Flask(__name__)
+app.config["SECRET_KEY"]= "87f4236d17bbabd54836d1f65e4e0c63"
 app.config["DEBUG"] = True
 
 
-@app.route('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    with open("templates/login.html") as file: 
-        data = file.read()
-    return data
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.username.data == "joarriag"  and form.password.data == "cisco123":
+            flash('Login requested for user {}, remember_me={}'.format(
+                form.username.data, form.remember_me.data))
+            return redirect(url_for('index'))
+        else:
+            flash('Login requested for user login Unsuccesful. Plese check username and password')
+    return render_template('loginTut.html', title='Sign In', form=form)
 
 @app.route('/widget')
 def Widget():
@@ -42,9 +51,22 @@ def respuestatelevisita():
         data = file.read()
     return data
 
-@app.route('/', methods=['GET'])
-def home():
-    return redirect(url_for("login"))
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")   
--- forms.py
+++ forms.py
@@ -1,15 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Email
-
-
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    email = StringField("Email",
-                            validators=[DataRequired(),Email()])
-    password = PasswordField("Password", 
-                            validators=[DataRequired()])
-    remember = BooleanField("Remember Me")
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
 
-    submit = SubmitField("Sign Up")
 

50b9b02cee39793ca0477e6722f88eb95e359c42
Uriel Arriaga <joarriag@cisco.com>
2020-04-19 01:52:50 -0500

    add unittest

---
+++ test_HTTP_Codes.py
@@ -0,0 +1,19 @@
+from app import app
+import unittest
+
+
+class MainTestCase(unittest.TestCase):
+
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get("/",content_type="html/text")
+        self.assertEqual(response.status_code, 302)
+    
+    def test_login (self):
+        tester = app.test_client(self)
+        response = tester.get("/login",content_type="html/text")
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()

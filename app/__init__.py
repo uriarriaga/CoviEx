5556a56b669d8c64f437233181c58b978de53d0d
ulix1808 <ulises.m.1808@gmail.com>
2020-04-26 20:49:15 -0500

    with expiredwidget and wired exprise time message

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_login import LoginManager
 
 
 app = Flask(__name__)
+
 app.config["SECRET_KEY"]= "87f4236d17bbabd54836d1f65e4e0c63"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False

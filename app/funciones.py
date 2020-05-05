5812979772e4bc7ecafafd1a166890fe89cfe347
Uriel Arriaga <joarriag@cisco.com>
2020-05-04 22:14:14 -0500

    fix generar Webex

--- app/funciones.py
+++ app/funciones.py
@@ -78,7 +78,7 @@ def sendSMS(contacto,token):
             sendWebexMsg(r.status_code,os.environ["idRoomTodos"])
 
 
-def generarWebex(listaNumeros=["5580663521"],correo="joarriag@cisco.com",nombre="teleconsulta",fecha=datetime.utcnow().timestamp())):
+def generarWebex(listaNumeros=["5580663521"],correo="joarriag@cisco.com",nombre="teleconsulta",fecha=datetime.utcnow().timestamp()):
     print(listaNumeros,correo,nombre,datetime.fromtimestamp(int(fecha)))
     timeForWebex = datetime.utcfromtimestamp(int(fecha)-18000).strftime("%m/%d/20%y %H:%M:00")
     actualTimePlusHR = str(datetime.utcnow().timestamp()+3600)

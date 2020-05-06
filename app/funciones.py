8f713c4083723dfa0b86e01d8accb95ea51b6e9d
Uriel Arriaga <joarriag@cisco.com>
2020-05-06 17:14:21 -0500

    send webex to debug

--- app/funciones.py
+++ app/funciones.py
@@ -92,7 +92,7 @@ def sendSMS(contacto,token):
 
 def generarWebex(listaNumeros=["5580663521"],correo="joarriag@cisco.com",nombre="teleconsulta"):
     fecha=datetime.utcnow().timestamp()
-     sendWebexMsg( listaNumeros,correo,nombre,datetime.fromtimestamp(int(fecha)-18000))
+    sendWebexMsg( listaNumeros,correo,nombre,datetime.fromtimestamp(int(fecha)-18000))
     timeForWebex = datetime.fromtimestamp(int(fecha)-18000).strftime("%m/%d/20%y %H:%M:00")
     actualTimePlusHR = str(datetime.utcnow().timestamp()+3600)
     invitados = db.session.query(GuestUser).filter(GuestUser.expirationTime<=datetime.utcnow().timestamp()).all()

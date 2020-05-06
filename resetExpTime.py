0e25460205bf0e99327ae923a7fd673cccc22619
Uriel Arriaga <joarriag@cisco.com>
2020-05-06 18:48:31 -0500

    upate reset Invitados

--- resetExpTime.py
+++ resetExpTime.py
@@ -3,8 +3,9 @@ from app import GuestUser
 from app.funciones import createJWT
 from datetime import datetime
 
-db.session.query(GuestUser).all()[0].expirationTime = 0
-db.session.query(GuestUser).all()[1].expirationTime = 0
+
+for invitado in db.session.query(GuestUser).all() :
+    invitado.expirationTime = 0
 db.session.commit()
 
 #createJWT()

17b39a028d21c0e287933134a496d7417450127b
ulix1808 <ulises.m.1808@gmail.com>
2020-05-14 00:53:13 -0500

    url_base BD productiva

--- app/funciones.py
+++ app/funciones.py
@@ -84,8 +84,8 @@ def sendAgendaSMS(contactos=["5580663521"],fecha="fecha en pruebas",tipo="tipo d
         sendSMS("+52"+contacto,texto)
 
 def sendWidgetSMS(contacto,token):
-    #texto = "Servicio de TeleConsulta INER. Para iniciar la videollamada favor de ingresar a la siguiente direccion: https://iner.teleconsulta.mx/widget?token=" + token 
-    texto = "Servicio de TeleConsulta INER. Para iniciar la videollamada favor de ingresar a la siguiente direccion: https://bdcc448e.ngrok.io/widget?token=" + token 
+    texto = "Servicio de TeleConsulta INER. Para iniciar la videollamada favor de ingresar a la siguiente direccion: https://iner.teleconsulta.mx/widget?token=" + token 
+    #texto = "Servicio de TeleConsulta INER. Para iniciar la videollamada favor de ingresar a la siguiente direccion: https://bdcc448e.ngrok.io/widget?token=" + token 
     sendSMS("+52"+contacto,texto)
 """
 def sendSMS(contacto,text):

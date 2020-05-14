2eb19f59638d492da627164834fe78928174da56
Uriel Arriaga <joarriag@cisco.com>
2020-05-13 20:02:52 -0500

    change widget to BrowserSDK

---
+++ app/static/scriptLlamadaSDK.js
@@ -0,0 +1,103 @@
+
+var webex = Webex.init();
+
+const token       = document.getElementById("token").innerHTML;
+console.log(token);
+const destination = document.getElementById('destino').innerHTML;
+console.log(destination);
+
+webex.once(`ready`, function() {
+    webex.authorization.requestAccessTokenFromJwt({jwt: token})
+      .then(() => {
+                  
+          webex.meetings.register()
+          .catch((err) => {
+            console.error(err);
+            alert(err);
+            throw err;
+          });
+
+          function bindMeetingEvents(meeting) {
+          meeting.on('error', (err) => {
+            console.error(err);
+          });
+
+          // Handle media streams changes to ready state
+          meeting.on('media:ready', (media) => {
+            if (!media) {
+              return;
+            }
+            if (media.type === 'local') {
+              document.getElementById('self-view').srcObject = media.stream;
+            }
+            if (media.type === 'remoteVideo') {
+              document.getElementById('remote-view-video').srcObject = media.stream;
+            }
+            if (media.type === 'remoteAudio') {
+              document.getElementById('remote-view-audio').srcObject = media.stream;
+            }
+          });
+
+          // Handle media streams stopping
+          meeting.on('media:stopped', (media) => {
+            // Remove media streams
+            if (media.type === 'local') {
+              document.getElementById('self-view').srcObject = null;
+            }
+            if (media.type === 'remoteVideo') {
+              document.getElementById('remote-view-video').srcObject = null;
+            }
+            if (media.type === 'remoteAudio') {
+              document.getElementById('remote-view-audio').srcObject = null;
+            }
+          });
+
+          // Of course, we'd also like to be able to leave the meeting:
+          document.getElementById('hangup').addEventListener('click', () => {
+            meeting.leave();
+          });
+          }
+
+          // Join the meeting and add media
+          function joinMeeting(meeting) {
+
+          return meeting.join().then(() => {
+            const mediaSettings = {
+              receiveVideo: true,
+              receiveAudio: true,
+              receiveShare: false,
+              sendVideo: true,
+              sendAudio: true,
+              sendShare: false
+            };
+
+            // Get our local media stream and add it to the meeting
+            return meeting.getMediaStreams(mediaSettings).then((mediaStreams) => {
+              const [localStream, localShare] = mediaStreams;
+
+              meeting.addMedia({
+                localShare,
+                localStream,
+                mediaSettings
+              });
+            });
+          });
+          }
+
+          document.getElementById('destination').addEventListener('submit', (event) => {
+          // again, we don't want to reload when we try to join
+          event.preventDefault();
+
+          return webex.meetings.create(destination).then((meeting) => {
+            // Call our helper function for binding events to meetings
+            bindMeetingEvents(meeting);
+
+            return joinMeeting(meeting);
+          })
+          .catch((error) => {
+            // Report the error
+            console.error(error);
+          });
+          });
+      })
+});

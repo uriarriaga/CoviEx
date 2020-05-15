9743f57887883c3558ca72c36f0666ed57ee557c
ulix1808 <ulises.m.1808@gmail.com>
2020-05-15 18:13:38 -0500

    no changes

--- app/static/scriptLlamadaSDK.js
+++ app/static/scriptLlamadaSDK.js
@@ -1,210 +1,122 @@
 
 const webex = window.webex = Webex.init();
 
-const token = document.getElementById("token").innerHTML;
+const token       = document.getElementById("token").innerHTML;
 console.log(token);
 const destination = document.getElementById('destino').innerHTML;
 console.log(destination);
 
-webex.once(`ready`, function () {
-    webex.authorization.requestAccessTokenFromJwt({ jwt: token })
-        .then(() => {
+webex.once('ready', function() {
+    webex.authorization.requestAccessTokenFromJwt({jwt: token})
+      .then(() => {
+                  
+          webex.meetings.register()
+          .catch((err) => {
+            console.error(err);
+            //alert(err);
+            throw err;
+          });
 
-            webex.meetings.register()
-                .catch((err) => {
-                    console.error(err);
-                    alert(err);
-                    throw err;
-                });
+          function bindMeetingEvents(meeting) {
+          meeting.on('error', (err) => {
+           // console.error("XXXXXXX PUTO EL Q_Ue LO LEA ");
+          });
 
-            function bindMeetingEvents(meeting) {
-                meeting.on('error', (err) => {
-                    console.error(err);
-                });
-
-                // Handle media streams changes to ready state
-                meeting.on('media:ready', (media) => {
-                    if (!media) {
-                        return;
-                    }
-                    if (media.type === 'local') {
-                        document.getElementById('self-view').srcObject = media.stream;
-                    }
-                    if (media.type === 'remoteVideo') {
-                        document.getElementById('remote-view-video').srcObject = media.stream;
-                    }
-                    if (media.type === 'remoteAudio') {
-                        document.getElementById('remote-view-audio').srcObject = media.stream;
-                    }
-                });
-
-                // Handle media streams stopping
-                meeting.on('media:stopped', (media) => {
-                    // Remove media streams
-                    if (media.type === 'local') {
-                        document.getElementById('self-view').srcObject = null;
-                    }
-                    if (media.type === 'remoteVideo') {
-                        document.getElementById('remote-view-video').srcObject = null;
-
-                    }
-                    if (media.type === 'remoteAudio') {
-                        document.getElementById('remote-view-audio').srcObject = null;
-                    }
-                });
-
-                // Of course, we'd also like to be able to leave the meeting:
-                document.getElementById('hangup').addEventListener('click', () => {
-                    meeting.leave();
-                });
-
-                return joinMeeting(meeting);
+          // Handle media streams changes to ready state
+          meeting.on('media:ready', (media) => {
+            if (!media) {
+              return;
             }
-
-
-            // Join the meeting and add media
-            function joinMeeting(meeting) {
-                // Get constraints
-                const constraints = {
-                    audio: document.getElementById('constraints-audio').checked,
-                    video: document.getElementById('constraints-video').checked
-                };
-
-                return meeting.join().then(() => {
-                    return meeting.getSupportedDevices({
-                        sendAudio: constraints.audio,
-                        sendVideo: constraints.video
-                    })
-                        .then(({ sendAudio, sendVideo }) => {
-                            const mediaSettings = {
-                                receiveVideo: constraints.video,
-                                receiveAudio: constraints.audio,
-                                receiveShare: false,
-                                sendShare: false,
-                                sendVideo: true,
-                                sendAudio: true
-                            };
-
-                            // Get our local media stream and add it to the meeting
-                            return meeting.getMediaStreams(mediaSettings).then((mediaStreams) => {
-                                const [localStream, localShare] = mediaStreams;
-
-                                meeting.addMedia({
-                                    localShare,
-                                    localStream,
-                                    mediaSettings
-                                });
-
-                            })
-
-                        })
-                });
+            if (media.type === 'local') {
+              document.getElementById('self-view').srcObject = media.stream;
             }
-            
-            document.getElementById('start-sending-audio').addEventListener('click', () => {
-                if (activeMeeting) {
-                  activeMeeting.unmuteAudio().then(() => {
-                    console.log("unMute");
-                  });
-                }
-              });
-        
-              document.getElementById('stop-sending-audio').addEventListener('click', () => {
-                if (activeMeeting) {
-                  activeMeeting.muteAudio().then(() => {
-                    console.log("Mute");
-                  });
-                }
-              });
-        
-              document.getElementById('start-sending-video').addEventListener('click', () => {
-                if (activeMeeting) {
-                  activeMeeting.unmuteVideo().then(() => {
-                    console.log("unMuteVideo");
-                  });
-                }
-              });
-        
-              document.getElementById('stop-sending-video').addEventListener('click', () => {
-                if (activeMeeting) {
-                  activeMeeting.muteVideo().then(() => {
-                    console.log("MuteVideo");
-                  });
-                }
-              });
+            if (media.type === 'remoteVideo') {
+              document.getElementById('remote-view-video').srcObject = media.stream;
+            }
+            if (media.type === 'remoteAudio') {
+              document.getElementById('remote-view-audio').srcObject = media.stream;
+            }
+          });
+
+          // Handle media streams stopping
+          meeting.on('media:stopped', (media) => {
+            // Remove media streams
+            if (media.type === 'local') {
+              document.getElementById('self-view').srcObject = null;
+            }
+            if (media.type === 'remoteVideo') {
+              document.getElementById('remote-view-video').srcObject = null;
+              
+            }
+            if (media.type === 'remoteAudio') {
+              document.getElementById('remote-view-audio').srcObject = null;
+            }
+          });
+
+          // Of course, we'd also like to be able to leave the meeting:
+          document.getElementById('hangup').addEventListener('click', () => {
 
 
-            document.getElementById('destination').addEventListener('submit', (event) => {
-
-                // again, we don't want to reload when we try to join
-                event.preventDefault();
-
-                return webex.meetings.create(destination).then((meeting) => {
-                    // Call our helper function for binding events to meetings
-                    activeMeeting = meeting;
-
-                    bindMeetingEvents(meeting);
-                    activeMeeting.muteVideo()
-
-                })
-                    .catch((error) => {
-                        // Report the error
-                        console.error(error);
-                    });
-            });
-        })
-});
-
-
-window.addEventListener('load', () => {
-    // Get elements from the DOM
-    const audio = document.getElementById('constraints-audio');
-    const video = document.getElementById('constraints-video');
-
-    // Get access to hardware source of media data
-    // For more info about enumerateDevices: https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/enumerateDevices
-    if (navigator && navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
-        navigator.mediaDevices.enumerateDevices()
-            .then((devices) => {
-                // Check if navigator has audio
-                const hasAudio = devices.filter(
-                    (device) => device.kind === 'audioinput'
-                ).length > 0;
-
-                // Check/uncheck and disable checkbox (if necessary) based on the results from the API
-                audio.checked = hasAudio;
-                audio.disabled = !hasAudio;
-
-                // Check if navigator has video
-                const hasVideo = devices.filter(
-                    (device) => device.kind === 'videoinput'
-                ).length > 0;
-
-                // Check/uncheck and disable checkbox (if necessary) based on the results from the API
-                video.checked = hasVideo;
-                video.disabled = !hasVideo;
-            })
-            .catch((error) => {
-                // Report the error
+            try {
+                  meeting.leave();
+                } catch (error) {
                 console.error(error);
+                    // expected output: ReferenceError: nonExistentFunction is not defined
+                    // Note - error messages will vary depending on browser
+                }
+        
+          });
+
+          console.log("llegamos al join meeting")
+          return joinMeeting(meeting);
+
+          }
+
+          // Join the meeting and add media
+          function joinMeeting(meeting) {
+          return meeting.join().then(() => {
+            const mediaSettings = {
+              receiveVideo: true,
+              receiveAudio: true,
+              receiveShare: false,
+              sendVideo: true,
+              sendAudio: true,
+              sendShare: false
+            };
+
+            // Get our local media stream and add it to the meeting
+            return meeting.getMediaStreams(mediaSettings).then((mediaStreams) => {
+              const [localStream, localShare] = mediaStreams;
+
+              meeting.addMedia({
+                localShare,
+                localStream,
+                mediaSettings
+              });
             });
-    }
-    else {
-        // If there is no media data, automatically uncheck and disable checkboxes
-        // for audio and video
-        audio.checked = false;
-        audio.disabled = true;
-
-        video.checked = false;
-        video.disabled = true;
-    }
-});
+          });
 
 
+          }
+
+          document.getElementById('destination').addEventListener('submit', (event) => {
 
 
+          
+          // again, we don't want to reload when we try to join
+          event.preventDefault();
 
-window.addEventListener('beforeunload', () => {
-    activeMeeting.leave()
-
+          return webex.meetings.create(destination).then((meeting) => {
+            // Call our helper function for binding events to meetings
+            bindMeetingEvents(meeting);
+            
+            
+          })
+          .catch((error) => {
+            // Report the error
+            console.error(error);
+           
+          });
+          });
+      })
 });

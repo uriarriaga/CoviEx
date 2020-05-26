e1b21ad536a1d68857bf8176a05027f0bd3b2a01
ulix1808 <ulises.m.1808@gmail.com>
2020-05-26 11:58:01 -0500

    error en codigo generaba doble slash

--- app/routes.py
+++ app/routes.py
@@ -432,8 +432,8 @@ def llamada():
     if tipo == "2":
         generarWebex(celulares,email, "Informe Medico " + nombre)
     if tipo == "3":
-        #generarWebex(celulares,email, "TeleVisita " + nombre)
-        print("echo")
+        generarWebex(celulares,email, "TeleVisita " + nombre)
+     
 
     #llamar funcoin envio de sms de uri todo en epoc
     # datetime
